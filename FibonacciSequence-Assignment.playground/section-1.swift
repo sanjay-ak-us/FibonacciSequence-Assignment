d4e10a058205467d8e0c96aa4906843fdcbbc2cc
sanjay-ak-us <sanjay.ak.us@gmail.com>
2015-01-03 14:42:10 +0100

    Assignment_Unit3_Lesson3.2

--- FibonacciSequence-Assignment.playground/section-1.swift
+++ FibonacciSequence-Assignment.playground/section-1.swift
@@ -9,20 +9,72 @@ import UIKit
 
 class FibonacciSequence {
     
-    let includesZero: Bool
-    let values: [Int]
+    let includesZero: Bool;
+    let values: [UInt];
     
-    init(maxNumber: Int, includesZero: Bool) {
+    init(maxNumber: UInt, includesZero: Bool) {
         self.includesZero = includesZero
         //TODO: Create an array which contains the numbers in the Fibonacci sequence, but don't add any numbers to the array which exceed the maxNumber. For example, if the maxNumber is 10 then the array should contain [0,1,1,2,3,5,8] because the next number is 13 which is higher than the maxNumber. If includesZero is false then you should not include the number 0 in the sequence.
+        var n_2:UInt = 0;
+        var n_1:UInt = 1;
+        if(!includesZero)
+        {
+            n_2 = 1;
+        }
+        var n:UInt = n_1 + n_2;
+        values = [n_2,n_1,n];
+        while(true)
+        {
+            n_2 = n_1;
+            n_1 = n;
+            var (n_temp, overflow) = UInt.addWithOverflow(n_1, n_2);
+            if(n_temp > maxNumber || overflow)
+            {
+                break;
+            }
+            n = n_temp;
+            values.append(n);
+        }
     }
     
-    init(numberOfItemsInSequence: Int, includesZero: Bool) {
+    init(numberOfItemsInSequence: UInt, includesZero: Bool) {
         self.includesZero = includesZero
-        //TODO: Create an array which contains the numbers in the Fibonacci sequence, and the array should contain this many items: numberOfItemsInSequence. For example, if numberOfItemsInSequence is 10 then the array should contain [0,1,1,2,3,5,8,13,21,34] if inlcudesZero is true, or [1,1,2,3,5,8,13,21,34,55] if includesZero is false.
+        //TODO: Create an array which contains the numbers in the Fibonacci sequence, but don't add any numbers to the array which exceed the maxNumber. For example, if the maxNumber is 10 then the array should contain [0,1,1,2,3,5,8] because the next number is 13 which is higher than the maxNumber. If includesZero is false then you should not include the number 0 in the sequence.
+        var n_2:UInt = 0;
+        var n_1:UInt = 1;
+        if(!includesZero)
+        {
+            n_2 = 1;
+        }
+        var n:UInt = n_1 + n_2;
+        values = [n_2,n_1,n];
+        var totalItems:UInt = 3;
+        for (totalItems; totalItems<numberOfItemsInSequence; ++totalItems)
+        {
+            n_2 = n_1;
+            n_1 = n;
+            var (n_temp, overflow) = UInt.addWithOverflow(n_1, n_2);
+            if(overflow)
+            {
+                println("\nOVERFLOW: The next number is too big to be stored in UInt!\n");
+                break;
+
+            }
+            n = n_temp;
+            values.append(n);
+        }
     }
 }
 
-let fibanocciSequence = FibonacciSequence(maxNumber:12345, includesZero: true)
+let fibanocciSequence = FibonacciSequence(maxNumber:12345, includesZero: true);
+for i in fibanocciSequence.values{
+    print("\(i) ");
+}
+println("\n");
 
-let anotherSequence = FibonacciSequence(numberOfItemsInSequence: 13, includesZero: true)
+
+let anotherSequence = FibonacciSequence(numberOfItemsInSequence: 22, includesZero: true)
+for a in anotherSequence.values{
+    print("\(a) ");
+}
+
